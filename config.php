2170454593de3becbdfc8c5abeac49f19a57509d
system56 <buzas.attila@hotmail.com>
2013-10-12 12:43:16 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+$Config["BasePath"] = "Core/";
+$Config["DBfile"] = "Core/db.php";
+ 
+$Config[MinYear] = 1300;
+ 
+//nyelv - magyar
+$Lang[Name] = "Név";
+$Lang[Team] = "Csapat";
+$Lang[Age] = "Kor";
+$Lang[Nationality] = "Nemzetiség";
+$Lang[ClubName] = "Csapat név";
+$Lang[Founded] = "Alapítva";
+$Lang[football] = "Foci";
+$Lang[New_team] = "Új csapat";
+$Lang[Home] = "Kezdőlap";
+$Lang[Player] = "Játékos";
+$Lang[Players] = "Játékosok";
+$Lang[Timeline] = "Idővonal";
+$Lang[When] = "Mikor";
+$Lang[OldTeam] = "Régi csapat";
+$Lang[NewTeam] = "Új Csapat";
+$Lang[Price] = "Ár";
+$Lang[No_data_to_display] = "Nincs megjeleníthető adat";
+$Lang['New'] = "Új";
+$Lang[Editing] = "Szerkesztés";
+$Lang[Add_new_player] = "Új játékos felvétele";
+$Lang[Add_new_club] = "Új klub felvétele";
+$Lang[Add_new_transfers] = "Új átigazolás felvétele";
+$Lang[Club] = "Klub";
+$Lang['Date'] = "Dátum";
+?>
